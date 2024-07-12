60ac300c67eca14827b781e2f9d7fa52600e5ec5
RishavSharma21 <rishavsharma21a@gmail.com>
2024-07-12 19:30:00 +0530

    12-07-2024

---
+++ src/Loops/DoWhileLoop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+        do{
+            System.out.print(i+" ");
+            i++;
+        } while (i<=10);
+
+        //we use do-while if we need to print at least 1 iteration
+    }
+}
---
+++ src/Loops/WhileLoop.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+       //While Loop
+        int i = 1;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
