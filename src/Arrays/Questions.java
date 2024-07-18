95c7c460e740c975e3ecc341e952e8fb15db52c7
RishavSharma21 <rishavsharma21a@gmail.com>
2024-07-18 19:52:44 +0530

    18-07-2024

--- src/Arrays/Questions.java
+++ src/Arrays/Questions.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Questions {
@@ -11,7 +12,7 @@ public class Questions {
         System.out.println();
     }
 
-    static int countOccurence(int[] arr,int x){
+    static int countOccurrence(int[] arr,int x){
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
@@ -21,10 +22,42 @@ public class Questions {
         return count;
     }
 
-    static int lastOccurence(int[] arr,int x){
-        
+    static int lastOccurrence(int[] arr,int x){
+        int lastindex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == x){
+                lastindex = i;
+            }
+        }
+        return lastindex;
     }
 
+    static int greaterElement(int[] arr,int x){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > x){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean isSorted(int[] arr){
+        boolean check = true;
+        for (int i = 1 ; i < arr.length ; i++) {
+            if(arr[i] < arr[i-1] ){
+                check = false;
+                break;
+            }
+        }
+      return check;
+    }
+
+    static int[] smallestAndlargestElement(int[] arr){
+        Arrays.sort(arr);
+        int[] ans = {arr[0] , arr[arr.length-1]};
+        return ans;
+    }
 
     public static void main(String[] args) {
 
@@ -39,11 +72,15 @@ public class Questions {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println("Enter the element x");
-        int x = sc.nextInt();
+//        System.out.print("Enter the element x:"+" ");
+//        int x = sc.nextInt();
 
-        System.out.println("Element X occurs "+ countOccurence(arr,x) + " times" );
-
-
+//        System.out.println("Element X occurs: "+ countOccurrence(arr,x) + " times" );
+//        System.out.println("Last Occurrence of X is: "+ lastOccurrence(arr,x));
+//        System.out.println("Elements greater than X is: "+ greaterElement(arr,x));
+//        System.out.println("Is Sorted: "+isSorted(arr));
+//        int[] ans = smallestAndlargestElement(arr);
+//        System.out.println("Smalllest: "+ans[0]);
+//        System.out.println("Largest: "+ans[1]);
     }
 }
