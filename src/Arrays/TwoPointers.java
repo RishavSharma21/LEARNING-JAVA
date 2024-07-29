4febb34edb6bd531f3b5907739a187d914d8cd47
RishavSharma21 <rishavsharma21a@gmail.com>
2024-07-29 14:40:04 +0530

    29-07-2024

---
+++ src/Arrays/TwoPointers.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class TwoPointers {
+
+    static void swap(int[] arr ,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    //Sort an Array consisting of only 0s and 1s
+
+    static void Sorting(int[] arr){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
